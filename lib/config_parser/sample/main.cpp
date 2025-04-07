43efe36403c5ba958771f7d744a33197ef2730cd
Clément Lagier <clement.lagier@epitech.eu>
2025-04-07 23:10:16 +0900

    [UPDATE] Modified the main.cpp to debug, and add another config file to debug

--- lib/config_parser/sample/main.cpp
+++ lib/config_parser/sample/main.cpp
@@ -2,13 +2,13 @@
 
 int main(void)
 {
-  std::string key = "KEY";
-  ConfigParser configParser("../sample/config.txt");
+  std::string key = "helloworld";
+  ConfigParser configParser;
 
+  configParser.loadConfig("../sample/config_with_whitespaces.txt");
   std::string value = configParser.get<std::string>(key);
 
-  if (value.empty())
-  {
+  if (value.empty()) {
     std::cerr << "Key not found or value is empty." << std::endl;
     return 1;
   }
