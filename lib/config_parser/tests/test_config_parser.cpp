227ae236ba8304df56203929d05463406596f283
Clément Lagier <clement.lagier@epitech.eu>
2025-04-07 23:09:07 +0900

    [ADD] Added some unit test using GTest

---
+++ lib/config_parser/tests/test_config_parser.cpp
@@ -0,0 +1,62 @@
+#include "ConfigParser.hpp"
+#include <gtest/gtest.h>
+
+#define CONFIG_FILE "../../tests/test_config.txt"
+#define CONFIG_FILE_WITH_SPACES "../../tests/test_config_with_whitespaces.txt"
+#define EMPTY_CONFIG_FILE "../../tests/test_empty_config.txt"
+
+TEST(ConfigParserTest, ParseConfigFile)
+{
+  std::string key = "key";
+  std::string value = "value";
+
+  ConfigParser parser;
+
+  parser.loadConfig(CONFIG_FILE);
+  std::string parsedValue = parser.get<std::string>(key);
+
+  EXPECT_EQ(parsedValue, value) << "Parsed value does not match expected value.";
+}
+
+TEST(ConfigParserTest, ParseConfigFileWithSpaces)
+{
+  std::string key = "helloworld";
+  std::string value = "helloworld";
+
+  ConfigParser parser;
+
+  parser.loadConfig(CONFIG_FILE_WITH_SPACES);
+  std::string parsedValue = parser.get<std::string>(key);
+
+  EXPECT_EQ(parsedValue, value) << "Parsed value does not match expected value.";
+}
+
+TEST(ConfigParserTest, EmptyConfigFile)
+{
+  ConfigParser parser;
+
+  // Attempt to load an empty config file
+  EXPECT_THROW(parser.loadConfig(EMPTY_CONFIG_FILE), ConfigParser::ConfigParserException) << "Expected runtime error when loading an empty config file.";
+}
+
+TEST(ConfigParserTest, InvalidKey)
+{
+  ConfigParser parser;
+
+  parser.loadConfig(CONFIG_FILE);
+
+  // Attempt to load a config file with an invalid key
+  EXPECT_THROW(parser.get<int>("invalid_key"), ConfigParser::ConfigParserException) << "Expected runtime error when accessing an invalid key.";
+}
+
+TEST(ConfigParserTest, ConfigWithComments)
+{
+  ConfigParser parser;
+
+  std::string key = "key_with_comment";
+  std::string value = "value_with_comment";
+  parser.loadConfig(CONFIG_FILE);
+  std::string parsedValue = parser.get<std::string>(key);
+
+
+}
