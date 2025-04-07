aade7620008ceaa419cf03d1fe28597640a45609
Clément Lagier <clement.lagier@epitech.eu>
2025-04-07 23:09:45 +0900

    [FIX] Fixed a typo

--- lib/config_parser/src/ConfigParser.cpp
+++ lib/config_parser/src/ConfigParser.cpp
@@ -1,10 +1,5 @@
 #include "ConfigParser.hpp"
 
-ConfigParser::ConfigParser(const std::string& filename)
-{
-  loadConfig(filename);
-}
-
 ConfigParser::~ConfigParser()
 {
     // Destructor
@@ -13,27 +8,32 @@ ConfigParser::~ConfigParser()
 int ConfigParser::loadConfig(const std::string &filename)
 {
   if (filename.empty())
-    throw ConfigParseException(std::string(EMPTY_FILE(filename)));
+    throw ConfigParserException(std::string(EMPTY_FILE(filename)));
 
   _filename = filename;
   std::ifstream file(_filename);
   if (!file.is_open())
-    throw ConfigParseException(std::string(FILE_NOT_FOUND(filename)));
+    throw ConfigParserException(std::string(FILE_NOT_FOUND(filename)));
 
   std::string line;
   while (std::getline(file, line)) {
     if (line.empty() || line[0] == '#') {
       continue; // Skip empty lines and comments
     }
+
+    // remove every whitespace
+    line.erase(std::remove_if(line.begin(), line.end(), ::isspace), line.end());
     size_t pos = line.find('=');
     if (pos == std::string::npos) {
-      throw ConfigParseException(std::string(INVALID_CONFIG_FORMAT(filename)));
+      throw ConfigParserException(std::string(INVALID_CONFIG_FORMAT(filename)));
     }
     std::string key = line.substr(0, pos);
     std::string value = line.substr(pos + 1);
     _configMap[key] = value;
     _configLines.push_back(line);
   }
+  if (_configMap.empty())
+    throw ConfigParserException(std::string(EMPTY_FILE(filename)));
   file.close();
   return 0;
 }
